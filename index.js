a7a5b8d5b7eca32fd9f00137f11b938035044072
Nicole Li <nicoleyatian@gmail.com>
2017-06-25 02:31:42 +0800

    edit for github page

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var server = app.listen(3000, function () {
 
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, '/node_modules')));
-app.use(express.static(path.join(__dirname, '/jQueryFileUpload')));
-
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
---
+++ js/app.js
@@ -0,0 +1,60 @@
+/*
+ * jQuery File Upload Plugin Angular JS Example
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2013, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ */
+
+/* jshint nomen:false */
+/* global window, angular */
+
+;(function () {
+    'use strict';
+
+    // var isOnGitHub = window.location.hostname === 'localhost',
+    var url = 'https://upload.wistia.com/?api_password=f5aeb57cd5eb1d5ffdbd47d72bcdf05bdca6672b189faebefba6b00e44d41c02';
+
+    angular.module('demo', [
+        'blueimp.fileupload'
+    ])
+        .config([
+            '$httpProvider', 'fileUploadProvider',
+            function ($httpProvider, fileUploadProvider) {
+                delete $httpProvider.defaults.headers.common['X-Requested-With'];
+                fileUploadProvider.defaults.redirect = window.location.href.replace(
+                    /\/[^\/]*$/,
+                    '/cors/result.html?%s'
+                );
+                    // Demo settings:
+                angular.extend(fileUploadProvider.defaults, {
+                    // Enable image resizing, except for Android and Opera,
+                    // which actually support image resizing, but fail to
+                    // send Blob objects via XHR requests:
+                    disableImageResize: /Android(?!.*Chrome)|Opera/
+                        .test(window.navigator.userAgent)
+                    // maxFileSize: 999000,
+                    // acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+                });
+                
+            }
+        ])
+
+        .controller('DemoFileUploadController', [
+            '$scope', '$http', '$filter', '$window',
+            function ($scope, $http) {
+                $scope.options = {
+                    url: url
+                };
+            }
+        ])
+
+        .component('uploader', {
+            controller: 'DemoFileUploadController',
+            templateUrl: '/component/uploader.template.html'
+        })
+
+}());
